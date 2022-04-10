1c395302af32797331727e594d87a317e9b86b21
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-04-10 15:25:05 +0200

    Updated the `Build.sh` script

--- Source/Building/Build.sh
+++ Source/Building/Build.sh
@@ -5,9 +5,10 @@ if [ ! -d '../../Build' ]; then
 fi
 
 cd ../ 
+
 python Setup.py build_ext --inplace # Convert Python to C
 
 # Cleaning
-mv Main.cpython-310-x86_64-linux-gnu.so ../Build/Pygame3D.so
+mv Main.cpython-310-x86_64-linux-gnu.so ../Build/PyGame3D.so
 rm -r -f build/
 rm -f Main.c
