926185408f0de1bccaa883314e910ca0746a4942
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2022-05-27 20:35:53 +0200

    Fixed everything by using PyWavefront

--- Source/Main.py
+++ Source/Main.py
@@ -11,8 +11,8 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-# Python files
-from objLoader import *
+# Model handling
+import pywavefront
 
 #endregion
 
@@ -39,11 +39,24 @@ def DrawCube():
     glEnd()
 """
 
+def Model():
+    glPushMatrix()
+    glScalef(*obj_scale)
+    glTranslatef(*obj_trans)
+
+    for mesh in obj.mesh_list:
+        glBegin(GL_TRIANGLES)
+        for face in mesh.faces:
+            for vertex_i in face:
+                glVertex3f(*obj.vertices[vertex_i])
+        glEnd()
+
+    glPopMatrix()
+
 def Update():
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
-    glCallList(obj.gl_list)
-    #DrawCube()
+    Model()
 
     pygame.display.flip() # Update the frames
     pygame.time.wait(10)
@@ -55,7 +68,20 @@ if __name__ == "__main__":
     display = (int(windowWidth), int(windowHeight))
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL) # Enables OpenGL with *double buffering*
 
-    obj = OBJ('Suzanne.obj', swapyz=False)
+    obj = pywavefront.Wavefront('Suzanne.obj', collect_faces=True)
+    obj_box = (obj.vertices[0], obj.vertices[0])
+
+    for vertex in obj.vertices:
+        min_v = [min(obj_box[0][i], vertex[i]) for i in range(3)]
+        max_v = [max(obj_box[1][i], vertex[i]) for i in range(3)]
+        obj_box = (min_v, max_v)
+
+    obj_trans = [-(obj_box[1][i]+obj_box[0][i])/2 for i in range(3)]
+
+    scaled_size = 5
+    obj_size = [obj_box[1][i]-obj_box[0][i] for i in range(3)]
+    max_obj_size = max(obj_size)
+    obj_scale = [scaled_size/max_obj_size for i in range(3)]
 
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
     glMatrixMode(GL_MODELVIEW)
